70df0cc337c3242e5a268d4c6efbf851cb4e65d5
Sam <samharries@gmail.com>
2015-03-30 17:05:00 +0100

    Doors now Open
    Still walk into them, and closing doesn't work yet though

--- Roguelike/Assets/Scripts/Action.cs
+++ Roguelike/Assets/Scripts/Action.cs
@@ -5,25 +5,31 @@ public static class Action
 {
     public static void OpenDoor(Transform thisLocation, Generation mapGenerator, Vector3 direction)
     {
-        if(IsItADoor(thisLocation.position + direction, mapGenerator))
+        Debug.Log("Testing");
+        Tile potentialLocation = mapGenerator.GetTile((int)thisLocation.position.x + (int)direction.x, (int)thisLocation.position.y + (int)direction.y);
+        if(IsItADoor(thisLocation.position + direction, mapGenerator, potentialLocation))
         {
-
+            mapGenerator.GetTile((int)potentialLocation.transform.position.x, (int)potentialLocation.transform.position.y).UpdateTileType(Tile.TileType.OpenDoor);
         }
     }
 
-    public static void CloseDoor()
+    public static void CloseDoor(Transform thisLocation, Generation mapGenerator, Vector3 direction)
     {
-
+        Tile potentialLocation = mapGenerator.GetTile((int)thisLocation.position.x + (int)direction.x, (int)thisLocation.position.y + (int)direction.y);
+        if (IsItADoor(thisLocation.position + direction, mapGenerator, potentialLocation))
+        {
+            mapGenerator.GetTile((int)potentialLocation.transform.position.x, (int)potentialLocation.transform.position.y).UpdateTileType(Tile.TileType.ClosedDoor);
+        }
     }
 
-    static bool IsItADoor(Vector3 newLocation, Generation mapGenerator)
+    static bool IsItADoor(Vector3 newLocation, Generation mapGenerator, Tile tile)
     {
-        Tile potentialLocation = mapGenerator.GetTile((int)newLocation.x, (int)newLocation.y);
-        if (potentialLocation.tileType == Tile.TileType.ClosedDoor)
+        if (tile.tileType == Tile.TileType.ClosedDoor)
         {
+            Debug.Log("Definitely a door!");
             return true;
         }
-        else if(potentialLocation.tileType == Tile.TileType.OpenDoor)
+        else if (tile.tileType == Tile.TileType.OpenDoor)
         {
             Debug.Log("Door already open");
         }
--- Roguelike/Assets/Scripts/PlayerControl.cs
+++ Roguelike/Assets/Scripts/PlayerControl.cs
@@ -20,6 +20,36 @@ public class PlayerControl : Actor
 	{
         if (canMove && !paralysed)
         {
+            if (Input.GetKey(KeyCode.O))
+            {
+                if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Keypad4)) // left
+                    Action.OpenDoor(location, mapGenerator, -Vector3.right);
+
+                if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Keypad8)) // up
+                    Action.OpenDoor(location, mapGenerator, Vector3.up);
+
+                if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Keypad6)) // right
+                    Action.OpenDoor(location, mapGenerator, Vector3.right);
+
+                if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.Keypad2)) // down
+                    Action.OpenDoor(location, mapGenerator, -Vector3.up);
+            }
+
+            if (Input.GetKey(KeyCode.C))
+            {
+                if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Keypad4)) // left
+                    Action.CloseDoor(location, mapGenerator, -Vector3.right);
+
+                if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Keypad8)) // up
+                    Action.CloseDoor(location, mapGenerator, Vector3.up);
+
+                if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Keypad6)) // right
+                    Action.CloseDoor(location, mapGenerator, Vector3.right);
+
+                if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.Keypad2)) // down
+                    Action.CloseDoor(location, mapGenerator, -Vector3.up);
+            }
+
             if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Keypad4)) // left
                 Movement.Move(location, mapGenerator, theMainCamera, instance, -Vector3.right);
 
@@ -45,25 +75,6 @@ public class PlayerControl : Actor
 
                 if (Input.GetKeyDown(KeyCode.Keypad9)) // up right
                     Movement.Move(location, mapGenerator, theMainCamera, instance, Vector3.right, Vector3.up);
-            }
-
-            if(Input.GetKeyDown(KeyCode.O))
-            {
-                //canMove = false;
-
-                if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Keypad4)) // left
-                    Action.OpenDoor(location, mapGenerator, -Vector3.right);
-
-                if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Keypad8)) // up
-                    Action.OpenDoor(location, mapGenerator, Vector3.up);
-
-                if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Keypad6)) // right
-                    Action.OpenDoor(location, mapGenerator, Vector3.right);
-
-                if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.Keypad2)) // down
-                    Action.OpenDoor(location, mapGenerator, -Vector3.up);
-
-                canMove = true;
             }
         }
 	}
